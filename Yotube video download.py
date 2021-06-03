b2a9f85e2fc9809a890628aca8e437a920f617f8
Kiranw24 <66425550+Kiranw24@users.noreply.github.com>
2021-06-03 13:41:45 +0530

    Add files via upload

---
+++ Yotube video download.py
@@ -0,0 +1,33 @@
+from tkinter import *
+
+from pytube import YouTube
+
+root = Tk()
+root.geometry('400x250')
+root.title('Youtube Video Downloader Application')
+root.configure(bg='magenta')
+
+def download():
+    try:
+        myVar.set('Downloading...')
+        root.update()
+        YouTube(link.get()).streams.first().download()
+        link.set('Video Downloaded Successfully')
+    except Exception as e:
+        myVar.set('Something went wrong')
+        root.update()
+        link.set('Please Enter Correct Link')
+
+Label(root,text='Welcome to Youtube\n Downloader Application',fg='red',font='Consolas 15 bold').pack()
+
+myVar = StringVar()
+
+myVar.set('Enter the link below')
+Entry(root,textvariable=myVar,width=25,font='Consolas 15 bold').pack(pady=10)
+
+link = StringVar()
+Entry(root,textvariable=link,width=40,font=('bold',15)).pack(pady=10)
+
+Button(root,text='Download Video',bg='yellow',font=('bold',15),command=download).pack()
+
+root,mainloop()
